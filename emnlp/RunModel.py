428b08bc478c6107f9486a212acf55f5fa920250
apollo2mars <apollo2mars@gmail.com>
2018-01-17 17:48:37 +0800

    Create RunModel.py

---
+++ emnlp/RunModel.py
@@ -0,0 +1,45 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# Usage: python RunModel.py modelPath inputPath"
+from __future__ import print_function
+import nltk
+# from util.preprocessing import addCharInformation, createMatrices, addCasingInformation
+from util.preprocessing import createMatrices
+from neuralnets.BiLSTM import BiLSTM
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage: python RunModel.py modelPath inputPath")
+    exit()
+
+modelPath = sys.argv[1]
+inputPath = sys.argv[2]
+
+with open(inputPath, 'r') as f:
+    text = f.read()
+
+
+# :: Load the model ::
+lstmModel = BiLSTM()
+lstmModel.loadModel(modelPath)
+
+
+# :: Prepare the input ::
+# sentences = [{'tokens': nltk.word_tokenize(sent)} for sent in nltk.sent_tokenize(text)]
+sentences = text
+# addCharInformation(sentenc
+# addCasingInformation(sentences)
+
+dataMatrix = createMatrices(sentences, lstmModel.mappings)
+
+# :: Tag the input ::
+tags = lstmModel.tagSentences(dataMatrix)
+
+
+# :: Output to stdout ::
+for sentenceIdx in range(len(sentences)):
+    tokens = sentences[sentenceIdx]['tokens']
+    tokenTags = tags[sentenceIdx]
+    for tokenIdx in range(len(tokens)):
+        print("%s\t%s" % (tokens[tokenIdx], tokenTags[tokenIdx]))
+    print("")
